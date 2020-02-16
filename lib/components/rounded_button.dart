492cc4ed666ac15142870aede420199b9e4e09d9
Boris Markovich <>
2020-02-16 10:59:24 -0500

    middle 189

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final Color buttonColor;
+  final String buttonText;
+  final Function screenNav;
+
+  RoundedButton({this.buttonColor, this.buttonText, @required this.screenNav});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: screenNav,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(buttonText),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import '../components/rounded_button.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -80,23 +82,12 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              buttonColor: Colors.lightBlueAccent,
+              buttonText: 'Log In',
+              screenNav: () {
+                Navigator.pushNamed(context, WelcomeScreen.id);
+              },
             ),
           ],
         ),
